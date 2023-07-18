3828667cb7657b44908ec2b3062ccf4293bf911e
alvaroflores <alvaro@brolty.com>
2023-07-18 12:22:47 -0400

    first commit

---
+++ encode_decode.py
@@ -0,0 +1,56 @@
+# Lab 6 by Alvaro Flores & Sarah Trainor
+
+def main():
+    # list of avail functions
+    options = [encode, decode, quit]
+    while running:
+        display_menu()
+        # promps for input from menu and runs selected choice
+        selection = int(input("Please enter an option: ")) - 1
+        options[selection]()
+
+
+def encode():
+    # prompts for password and encodes it.
+    global og_password, encoded_pw
+    newlist = []
+    og_password = input("Please enter your password to encode:")
+    for l in og_password:
+        if int(l) < 7:
+            newlist.append(str(int(l) + 3))
+        else:
+            newlist.append(str(int(l) - 7))
+    encoded_pw = ''.join(newlist)
+    print("Your password has been encoded and stored!")
+    return encoded_pw
+
+
+def decode():
+    # prints out encoded pw and decoded pw
+    pass
+    # print(
+    #     f'The encoded password is {encoded_pw}, and the original password is {og_password}')
+    # return
+
+
+def exit():
+    # terminate program
+    running = False
+
+
+def display_menu():
+    # displays the menu
+    print("""
+        Menu
+        ---------
+        1. Encode
+        2. Decode
+        3. Quit
+        """)
+
+
+if __name__ == '__main__':
+    running = True
+    og_password = None
+    encoded_pw = None
+    main()
